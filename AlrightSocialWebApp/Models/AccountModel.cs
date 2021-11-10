99a37a2cffef2d2c7308717293e33a132872a48f
Lê Bùi Dĩ Hoà <lebuidihoa257@gmail.com>
2021-11-10 16:30:00 +0700

    1

---
+++ AlrightSocialWebApp/Models/AccountModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlrightSocialWebApp.Models
+{
+    public class AccountModel
+    {
+        [Required(ErrorMessage = "Email không được để trống")]
+        [Key]
+        public string Email { set; get; }
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        public string Password { set; get; }
+    }
+}
---
+++ AlrightSocialWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlrightSocialWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
